541e5da93351220c4f67dc5956cfd22a2d052027
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-31 15:52:01 +0400

    feat(services): Updating SchoolService.java file

--- src/main/java/com/codeline/mysqldemo/repositories/SchoolRepository.java
+++ src/main/java/com/codeline/mysqldemo/repositories/SchoolRepository.java
@@ -2,8 +2,18 @@ package com.codeline.mysqldemo.repositories;
 
 import com.codeline.mysqldemo.entities.School;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SchoolRepository extends JpaRepository<School, Long> {
+    @Query("SELECT s FROM School s WHERE s.isActive=true")
+    List<School> getAllSchool();
+
+    //abc is a name of parameter
+    @Query("SELECT s FROM School s WHERE s.isActive=true AND s.id=:abc")
+    School getById(@Param("abc") Long id); //replacing the id into abc
 }
--- src/main/java/com/codeline/mysqldemo/services/SchoolService.java
+++ src/main/java/com/codeline/mysqldemo/services/SchoolService.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 //the purpose of the service is to do business logic
 @Service
@@ -30,4 +32,46 @@ public class SchoolService {
         school = schoolRepository.save(school); //used to save the data into the database
         return school.getId(); //it return the output
     }
+
+    //GET ALL
+    public List<School> getAllSchools(){
+        return schoolRepository.getAllSchool();
+    }
+
+    //GET BY ID - to return one school
+    public School getById(Long id){
+        Optional <School> school =  schoolRepository.findById(id);
+        //condition to check the optional function
+        if(school.isPresent() && school.get().getIsActive()){
+            return school.get();
+        }
+        return new School();
+    }
+
+    //Update
+    public School updatedSchool(Long id, String updateName, String updateLocation){
+        School schoolToUpdate = schoolRepository.getById(id);
+        if(schoolToUpdate == null){
+            return new School(); //this return an empty school
+        }
+        schoolToUpdate.setUpdatedDate(new Date());
+        schoolToUpdate.setName(updateName);
+        schoolToUpdate.setLocation(updateLocation);
+        schoolToUpdate = schoolRepository.save(schoolToUpdate); //used to save the data into the database
+        return schoolToUpdate;
+    }
+
+    //delete
+    public Boolean deleteById(Long id) {
+        School deleteSchool = schoolRepository.getById(id);
+        if(deleteSchool == null){
+            return false; //this return an empty school
+        } else {
+            deleteSchool.setIsActive(false);
+            deleteSchool.setUpdatedDate(new Date());
+            schoolRepository.save(deleteSchool);
+            return true;
+        }
+    }
+
 }
