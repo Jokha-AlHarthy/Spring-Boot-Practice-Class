e9a63a24d93313bee0100f707e2199a321f7c031
Jokha-AlHarthy <jokha.hamed.official@gmail.com>
2026-08-31 14:15:13 +0400

    feat(repoisitories): Adding SchoolRepository.java file

---
+++ src/main/java/com/codeline/mysqldemo/repositories/SchoolRepository.java
@@ -0,0 +1,9 @@
+package com.codeline.mysqldemo.repositories;
+
+import com.codeline.mysqldemo.entities.School;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Long> {
+}
